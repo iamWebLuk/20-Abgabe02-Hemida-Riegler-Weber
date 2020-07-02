d58b57b24f06b49797cca86868ffac7a55c7b258
RegalFenster <48275164+RegalFenster@users.noreply.github.com>
2020-07-02 15:04:35 +0200

    change constructor name, remove semicolon so code works

--- src/test/java/at/fhj/iit/StringQueueTest.java
+++ src/test/java/at/fhj/iit/StringQueueTest.java
@@ -3,10 +3,18 @@ package at.fhj.iit;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class StringQueueTest {
+    private Queue q;
 
-   // TODO Write your own tests
+
+    @Test
+    @DisplayName("Testing something")
+    public void testsome(){
+
+    }
+    // TODO Write your own tests
 
 }
