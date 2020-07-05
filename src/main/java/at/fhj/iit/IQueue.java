e5ae761b24c6cc8035fe57d2ddba600ed496d6e4
Islam Hemida <isigamer@hotmail.com>
2020-07-05 23:28:15 +0200

    added optional Task

--- src/main/java/at/fhj/iit/IQueue.java
+++ src/main/java/at/fhj/iit/IQueue.java
@@ -5,7 +5,7 @@ public interface IQueue<Object> {
    * Adds the element obj to the queue.
    * If the addition is successful, the method returns true else false.
    *
-   * @param obj descirbe blalbal
+   * @param obj the element to be added to the queue
    * @return true if succesfull otherwiser false
    */
   public abstract boolean offer(Object obj);
@@ -14,7 +14,7 @@ public interface IQueue<Object> {
    * Returns the head (first) element and also deletes it. That is, we cannot get it again.
    * If no element exists (when queue is empty), the method returns null.
    *
-   * @return
+   * @return true if succesfull otherwiser false
    */
   public abstract Object poll();
 
@@ -22,7 +22,7 @@ public interface IQueue<Object> {
    * It also returns and deletes the head element like poll(), but with a small difference.
    * This method throws NoSuchElementException if the queue is empty.
    *
-   * @return
+   * @return  head element of the Queue
    */
   public abstract Object remove();
 
@@ -30,7 +30,7 @@ public interface IQueue<Object> {
    * Returns the head element but it does not delete it. That is, we can get it again.
    * Returns null when the queue is empty.
    *
-   * @return
+   * @return  head element of the Queue
    */
   public abstract Object peek();
 
@@ -38,7 +38,7 @@ public interface IQueue<Object> {
    * It works similar to peek() but with a small difference (returns but does not delete the element).
    * It throws NoSuchElementException when the queue is empty.
    *
-   * @return
+   * @return  head element of the Queue
    */
   public abstract Object element();
 }
---
+++ src/site/markdown/queuedetail.md
@@ -0,0 +1,11 @@
+##Wann benutzt man eine Queue
+
+Eine Queue benutzt man wenn man etwas genau so in der Reihenfolge ausgegeben haben will, wie man es befüllt.
+
+##Was sind Generics
+
+Mit Generics ("Generizität", "generische Elemente") werden im Java-Umfeld Sprachmittel bezeichnet, mit denen Klassen und Methoden mit Typparametern parametrisiert werden können, um Typsicherheit trotz generischer Programmierung zu ermöglichen. Generics werden nur während der Kompilierung ausgewertet.
+
+##Was ist eine Code Coverage bei Source Code Tests?
+
+Auf Deutsch übersetzt bedeutet es Test Abdeckung. Man meint damit die getätigten Tests im Verhältnis zu den theoretisch möglichen Tests
